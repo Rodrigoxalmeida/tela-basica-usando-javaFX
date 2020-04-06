93b6e271175b81e2e3ed51862e6d4afdf794867c
Rodrigoxalmeida <Rodrigox.almeida@yahoo.com.br>
2020-04-05 22:13:48 -0300

    Janela Melhorada

--- src/view/LoginApp.java
+++ src/view/LoginApp.java
@@ -3,6 +3,7 @@ package view;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
@@ -18,6 +19,7 @@ public class LoginApp extends Application {
 		AnchorPane pane = new AnchorPane();
 		
 		pane.setPrefSize(400, 300); // primeiro paramentro largura(Width) e o segundo altura(Height)
+		stage.setTitle("Cadastro");// titulo para janela
 		// codigo css para o painel
 		pane.setStyle("-fx-background-color: linear-gradient(from 0% 0% to 100% 100%, blue 0%, silver 100%);");	
 		
@@ -28,17 +30,20 @@ public class LoginApp extends Application {
 		PasswordField txSenha = new PasswordField();
 		txSenha.setPromptText("Digite a senha");
 		
+		Label tex = new Label("Desenvolvimento: JavaFX");
+		Label titulo = new Label("LoginApp");		
+		
 		// Criando os botoes entrar e sair
 		Button btEntrar = new Button("Entrar");
 		Button btSair = new Button("Sair");		
 		
 		// colocando os componentes no painel usando o metodo getChildren().addAll(Todos
 		// os componentes como parâmetro)
-		pane.getChildren().addAll(txLogin, txSenha, btEntrar, btSair);
+		pane.getChildren().addAll(txLogin, txSenha, btEntrar, btSair,tex,titulo);
 
 		// criando uma cena e passando como argumento o painel.
 		Scene scene = new Scene(pane);
-
+		
 		// depois de criar a cena, tem qu indicar qual tela vai ser usada
 		// essa stage(cena e passada no proprio metodo start)
 		stage.setScene(scene);
@@ -61,7 +66,10 @@ public class LoginApp extends Application {
 		btEntrar.setLayoutY(150);
 		btSair.setLayoutX((pane.getWidth() - btSair.getWidth()) / 2);
 		btSair.setLayoutY(200);
-
+		tex.setLayoutX((pane.getWidth() - tex.getWidth())/2);
+		tex.setLayoutY(250);
+		titulo.setLayoutX((pane.getWidth() - titulo.getWidth())/2);
+		titulo.setLayoutY(10);
 	}
 
 	public static void main(String[] args) {
