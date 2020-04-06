b53ebc49b3a92bfa2ea4f3b2bf736610b42fa121
Rodrigoxalmeida <Rodrigox.almeida@yahoo.com.br>
2020-04-05 21:06:52 -0300

    JanelaFuncionando

--- src/view/LoginApp.java
+++ src/view/LoginApp.java
@@ -1,5 +1,37 @@
 package view;
 
-public class LoginApp {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class LoginApp extends Application {
+
+	@Override
+	public void start(Stage stage) throws Exception {
+
+		// criando um painel para os componentes
+		// anchorPane da total liberdade na localização dos componentes
+		AnchorPane pane = new AnchorPane();
+		
+		pane.setPrefSize(400, 300); // primeiro paramentro largura(Width) e o segundo altura(Height)
+		// criando uma cena e passando como argumento o painel.
+		Scene scene = new Scene(pane);
+		
+		// depois de criar a cena, tem qu indicar qual tela vai ser usada
+		// essa stage(cena e passada no proprio metodo start)
+		stage.setScene(scene);
+		
+		// depois abre a cena com metodo show()
+		stage.show();
+		
+		
+
+		
+	}
+	
+	public static void main(String[] args) {
+		launch(args); // executando o codigo
+	}
 
 }
